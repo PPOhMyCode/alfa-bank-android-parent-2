d6416630122d863e84a492667f7212fd74f7c985
DanilBychkov <Danil.Bychkov@urfu.me>
2022-11-03 14:07:05 +0500

    create mobile

---
+++ app/src/main/java/com/example/alfa_bank_android_app_parent_2/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.alfa_bank_android_app_parent_2.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
